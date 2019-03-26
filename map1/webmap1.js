b02782c4d9a2ef683ab621e7bcdd582c9c257cbd
erolknaus <48488099+erolknaus@users.noreply.github.com>
2019-03-26 11:36:59 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,4 +1,8 @@
 <script src="script.js"></script>
   Baton Rouge = L.map('BatonRouge').setView([30.4515° N, 91.1871° W)
-L.tileLayer(
-  
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.)
+let LSU = L.marker ([30.4133° N, 91.1800° W])
+let BRCC = L.polygon([
+  [30.3241, 91.0944],
+  [30.3229, 91.0820],
+  [30.3107, 91.0850]  
