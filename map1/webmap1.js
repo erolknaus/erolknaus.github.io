7bfe8fb2b220a562885e3db8af48bb0603d36140
erolknaus <48488099+erolknaus@users.noreply.github.com>
2019-03-26 11:49:13 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,3 +1,8 @@
 <script src="script.js"></script>
   Baton Rouge = L.map('Baton Rouge').setview([30.4515 N, 91.1871 W)
 L.tileLayer('http://leaflet-extras.github.io/leaflet-providers/preview/#filter=Esri')
+let LSU = L.marker ([30.4133° N, 91.1800° W])
+let BRCC = L.polygon([
+  [30.3241, 91.0944],
+  [30.3229, 91.0820],
+  [30.3107, 91.0850]
