a27b9ff9282dec80ae78fe56884bded2c8391dc0
erolknaus <48488099+erolknaus@users.noreply.github.com>
2019-04-25 11:48:50 -0500

    Update webmap3.js

--- Map3/webmap3.js
+++ Map3/webmap3.js
@@ -4,9 +4,9 @@ L.tileLayer(basemapUrl).addTo(myMap)
 let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
 jQuery.getJSON(stateDemographicsUrl, function (data) {
   let stateStyle = function (feature) {
-    let age = feature.properties.MED_AGE // get the current state's Median Age attribute
+    let size = feature.properties.SQMI // get the current state's Average Size attribute
     let stateColor = 'olive' // let the initial color be a darker green
-    if ( age < 38 ) { stateColor = 'green' } // if the state's median age is less than the average, color it a lighter green
+    if ( size < 581369.17 ) { stateColor = 'green' } // if the state's median size is less than the average, color it a lighter green
     return {
       color: stateColor, //use the color variable above for the value
       weight: 1,
@@ -15,8 +15,8 @@ jQuery.getJSON(stateDemographicsUrl, function (data) {
   }
   let onEachFeature = function (feature, layer) {
      let name = feature.properties.STATE_NAME
-     let size = feature.properties.MED_SIZE
-     layer.bindPopup('Median size of ' + name + ': ' + size + '<br>National average: ')
+     let size = feature.properties.SQMI
+     layer.bindPopup('Average size of ' + name + ': ' + size + '<br>National average: ')
    }
   let geojsonOptions = {
     style: stateStyle,
