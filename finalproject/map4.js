d39ff43f654bbd3c082c49c54eeaeac01e55c339
erolknaus <48488099+erolknaus@users.noreply.github.com>
2019-05-03 12:41:45 -0500

    Update map4.js

--- finalproject/map4.js
+++ finalproject/map4.js
@@ -1,12 +1,12 @@
-let floridamap = L.map('map3').setView([32.18, -99.14], 4)
+let floridamap = L.map('finalproject').setView([32.18, -99.14], 4)
 let basemapUrl = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png'
 L.tileLayer(basemapUrl).addTo(floridamap)
 let floridaDemographicsUrl = 'https://opendata.arcgis.com/datasets/003e10bdf5054162b27c65db3ebc3d07_0.geojson'
 jQuery.getJSON(floridaDemographicsUrl, function (data) {
-  let stateStyle = function (feature) {
-   let TotalPopul = feature.properties.TotalPopul // get the current state's Population attribute
+  let zipStyle = function (feature) {
+   let TotalPopul = feature.properties.TotalPopul // get the current zip's Population attribute
    let zipColor = 'Red' // let the initial color be red
-   if ( TotalPopul < 20000 ) { stateColor = 'green' } // if the state's size is less than the national average, color it a lighter green
+   if ( TotalPopul < 20000 ) { stateColor = 'green' } // if the zip's population is less than the state average, color it green
    return {
      color: stateColor, //use the color variable above for the value
      weight: 1,
@@ -14,12 +14,12 @@ jQuery.getJSON(floridaDemographicsUrl, function (data) {
     }
   }
   let onEachFeature = function (feature, layer) {
-     let name = feature.properties.STATE_NAME
+     let name = feature.properties.FDI
      let size = feature.properties.SQMI
      layer.bindPopup('Average size of ' + name + ': ' + size + '<br>National average: ')
    }
   let geojsonOptions = {
-    style: stateStyle,
+    style: zipStyle,
     onEachFeature: onEachFeature
   }
   L.geoJSON(data, ).addTo(floridamap)
